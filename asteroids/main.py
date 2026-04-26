f42c88382e7157de9a97e2fa421b064b2343b642
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-04-26 13:23:52 -0700

    fps additon and controlled

--- asteroids/main.py
+++ asteroids/main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -14,6 +16,8 @@ def main():
 
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
